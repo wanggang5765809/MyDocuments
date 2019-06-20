51b998c713f921ed360d75c31172ffdb1076d7ed
wanggang5765809 <wanggang5765809@163.com>
2019-06-20 14:59:58 +0800

    更新mysql安装脚本，添加8.0的初始化 sql优化笔记

--- LinuxShell/installMysql.sh
+++ LinuxShell/installMysql.sh
@@ -9,4 +9,8 @@ mkdir -p /data/mysql/mysql3306/{data,logs,tmp}
 chown -R mysql:mysql /data
 cd /usr/local/mysql/bin/
 ./mysqld --initialize
+#指定配置文件初始化
+./mysqld --initialize --user=mysql --basedir=/usr/local/software/mysql7/ --datadir=/data/mysql/mysql3307/data/
+#mysql8.0初始化
+./mysqld --initialize --user=mysql --lower-case-table-names=1 --basedir=/usr/local/mysql/ --datadir=/data/mysql/mysql3306/data/
 ./mysqld --defaults-file=/etc/my.cnf &
